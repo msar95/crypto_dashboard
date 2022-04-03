8cf1378c6f78833fab26d651bb074573c5094685
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-03 16:06:56 +0200

    Style AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,15 +1,33 @@
 
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 const Bar = styled.div`
     display: grid;
     grid-template-columns: 180px auto 100px 100px;
+    margin-bottom: 50px;
 `
+const Logo = styled.div`
+    font-size: 1.5em;
+`
+const ControlButtonElem = styled.div`
+    cursor:pointer;
+    text-transform: capitalize;
+    ${props =>  props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;
+`}
+`
+function ControlButton ({name, active}){
+    return (
+        <ControlButtonElem active={active}>
+            {name}
+        </ControlButtonElem>
+    )
+}
 export default function (){ // Bar -> ist der Container des CSS-Grid
     return <Bar>
-        <div>CryptoDash</div>
+        <Logo>CryptoDash</Logo>
         <div/>
-        <div>Dashboard</div>
-        <div>Settings</div>
+        <ControlButton active name="dashboard"/>
+        <ControlButton active name="settings"/>
     </Bar> // die Divs sind die Children
 }
