d386073275da36c850b697a8c17dc01f96e219ba
Marius Albeser <albeser.marius@gmail.com>
2022-04-04 11:30:38 +0200

    Update AppBAr.js

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -5,11 +5,14 @@ const Bar = styled.div`
     display: grid;
     grid-template-columns: 180px auto 100px 100px;
 `
+const NavElem = styled.div`
+    cursor: pointer;
+`
 export default function (){ // Bar -> ist der Container des CSS-Grid
     return <Bar>
         <div>CryptoDash</div>
         <div/>
-        <div>Dashboard</div>
-        <div>Settings</div>
+        <NavElem>Dashboard</NavElem>
+        <NavElem>Settings</NavElem>
     </Bar> // die Divs sind die Children
 }
